e5f6d810bd6b14c7db1f50eaba42c2cdd6c49f1f
nandnimohan <nandnimohan10@gmail.com>
2025-06-24 15:36:06 +0530

    Update constants.ts with latest onboarding changes

--- src/components/onboard/constants.ts
+++ src/components/onboard/constants.ts
@@ -1,5 +1,5 @@
 
 export const languageOptions = ['English', 'Spanish', 'French', 'German', 'Italian', 'Portuguese', 'Mandarin', 'Japanese'];
-export const feeRanges = ['$250-500', '$500-1000', '$1000-2000', '$2000-5000', '$5000+'];
+export const feeRanges = ['Rs 20k-30k', 'Rs 30k-50k', 'Rs 50k-70k', 'Rs 70k-90k', 'Rs 90k+'];
 export const experienceLevels = ['Beginner (0-2 years)', 'Intermediate (3-5 years)', 'Advanced (5-10 years)', 'Expert (10+ years)'];
 export const availabilityOptions = ['Weekdays', 'Weekends', 'Evenings', 'Holidays', 'Travel Required'];
